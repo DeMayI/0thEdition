31afbe0b93da7a32bf0d3667a564cbd0ce83b670
DeMayI <gigabyteruler@gmail.com>
2026-03-12 02:52:35 -0700

    Fixed missing slash in base

--- starlight/astro.config.mjs
+++ starlight/astro.config.mjs
@@ -7,7 +7,7 @@ import markdoc from '@astrojs/markdoc';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://demayi.github.io',
-    base: '0thEdition',
+    base: '/0thEdition',
     integrations: [starlight({
         title: 'Warhammer 0th Edition',
         social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/DeMayI/0thEdition' }],
